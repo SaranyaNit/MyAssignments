ac9f9afffd2794f5b3190c8ed82e8bcef2ca01ae
SaranyaNit <saranyagokul11@gmail.com>
2025-10-17 20:41:06 +0530

    Add files via upload

---
+++ numberType.js
@@ -0,0 +1,9 @@
+let num = 100;
+if (num >= 0)
+    console.log (`The number ${num} is positive`)
+else if (num < 0)
+    console.log (`The number ${num} is negative`)
+else
+    console.log (`The number ${num} is zero`)
+
+
