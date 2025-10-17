abe68252368054bc841dae65e7469b7eacd7c5c8
SaranyaNit <saranyagokul11@gmail.com>
2025-10-17 20:16:17 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,14 @@
+let userName = "Saranya"
+let organization = "TransUnion"
+let contactNumber = 6381009287n
+let knowsAutomation = true
+let usesPlaywright 
+
+console.log (userName +" "+ typeof userName)
+console.log (organization +" "+ typeof organization)
+console.log (contactNumber +" "+ typeof contactNumber)
+console.log (knowsAutomation +" "+ typeof knowsAutomation)
+console.log (usesPlaywright +" "+ typeof usesPlaywright)
+
+
+
